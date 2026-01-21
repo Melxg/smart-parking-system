b44328e7c24a90298c678999af31c2e89256b5c4
Hellyrj <hellyhussein1@gmail.com>
2026-01-22 03:19:41 +0900

    parking space database

--- parkingspace/src/app.js
+++ parkingspace/src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
+import searchRoutes from "./routes/searchRoutes.js";
+import parkingRoutes from "./routes/parkingRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/auth", authRoutes);
-
+app.use("/parking", parkingRoutes);
+app.use("/search", searchRoutes);
 export default app;
+
